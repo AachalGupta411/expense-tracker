0ddddafb99afd7ec35f750c4fcc37ab7a0a77e40
Aachal Gupta <aachalgupta@Aachals-MacBook-Air.local>
2026-03-28 10:16:48 +0530

    feat: add SessionLocal for database transactions

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from app.config import settings
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
     settings.DATABASE_URL,
     pool_pre_ping=True
+)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
